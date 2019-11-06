c61abf76a80bdde8d390ffa3e027fff2520d8974
Joshua <jballard@unca.edu>
2019-11-06 17:23:02 -0500

    cleaned up a bit more

--- app/src/main/java/parallelcodes/mysqlapp/MainActivity.java
+++ app/src/main/java/parallelcodes/mysqlapp/MainActivity.java
@@ -90,47 +90,6 @@ private class MyTask extends AsyncTask<String,Void,String>{
 }//mytask
 
 
-/*
-        private class ConnectMySql extends AsyncTask<String, Void, String> {
-        String res = "";
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            Toast.makeText(MainActivity.this, "Please wait...", Toast.LENGTH_SHORT)
-                    .show();
-
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                Class.forName("org.mariadb.jdbc.Driver");
-                Connection con = DriverManager.getConnection(url, user, pass);
-                System.out.println("Databaseection success");
-
-                String result = "Database Connection Successful\n";
-                Statement st = con.createStatement();
-                ResultSet rs = st.executeQuery("select distinct Heading from Test");
-                ResultSetMetaData rsmd = rs.getMetaData();
-
-                while (rs.next()) {
-                    result += rs.getString(1).toString() + "\n";
-                }
-                res = result;
-            } catch (Exception e) {
-                e.printStackTrace();
-                res = e.toString();
-            }
-            return res;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            txtData.setText(result);
-        }
-    }
-
-*/
 
 }
