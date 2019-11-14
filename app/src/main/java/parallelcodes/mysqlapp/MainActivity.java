62f0a0b40dc98414df8fae0bb333b3765d0de86c
andrerucker <arucker@unca.edu>
2019-11-14 12:32:44 -0500

    11-14-2019
    Writing to database

--- app/src/main/java/parallelcodes/mysqlapp/MainActivity.java
+++ app/src/main/java/parallelcodes/mysqlapp/MainActivity.java
@@ -2,38 +2,41 @@ package parallelcodes.mysqlapp;
 
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class MainActivity  extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, OnMapReadyCallback {
 
     private static final String url = "jdbc:mariadb://10.123.21.91:3306/myDB";
     private static final String user = "BallardPi";
     private static final String pass = "BallardPi";
+    private static boolean connected = false;
+    private static boolean update = false;
+    MyTask myTask = new MyTask(this);
 
     private GoogleMap mMap;
 
@@ -42,6 +45,7 @@ public class MainActivity  extends AppCompatActivity implements GoogleMap.OnMyLo
 
     // http://10.123.21.91/phpmyadmin/index.php
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,10 +57,9 @@ public class MainActivity  extends AppCompatActivity implements GoogleMap.OnMyLo
 
             @Override
             public void onClick(View v) {
-                MyTask myTask = new MyTask();
+                // myTask = new myTask();
+                //    Try this to update one line in database at a time
                 myTask.execute();
-// TODO Auto-generated method stub
-
             }
         });
         btnClear.setOnClickListener(new View.OnClickListener() {
@@ -65,10 +68,6 @@ public class MainActivity  extends AppCompatActivity implements GoogleMap.OnMyLo
                 txtData.setText("");
             }
         });
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        //mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -84,58 +83,90 @@ public class MainActivity  extends AppCompatActivity implements GoogleMap.OnMyLo
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // public Location getMyLocation
-        // Add a marker in Sydney and move the camera
-        //LatLng UNCA_Quad = new LatLng(35.616314, -82.56732);
-        //mMap.addMarker(new MarkerOptions().position(UNCA_Quad).title("Marker in Asheville quad"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(UNCA_Quad));
         mMap.setMyLocationEnabled(true);
         mMap.setOnMyLocationButtonClickListener(this);
         mMap.setOnMyLocationClickListener(this);
 
     }
 
-    private class MyTask extends AsyncTask<String,Void,String>{
+    public final class MyTask extends AsyncTask<String,Void,String>
+    {
         String res = "";
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        Location loc = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+
+        MainActivity help;
+
+        private MyTask(MainActivity main){
+            help = main;
+        }
+
         @Override
         protected String doInBackground(String... strings) {
-            try {
-                Class.forName("org.mariadb.jdbc.Driver");
+
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            String result = "Database Connection Successful\n";
+            Connection conn = null;
+
                 try {
-                    Connection conn = DriverManager.getConnection(url,user,pass);
-                    System.out.println("Database connection success");
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    try {
+                        conn = DriverManager.getConnection(url, user, pass);
+                        System.out.println("Database connection success");
 
-                    // the mysql insert statement
-                    String query = " insert into Test (Heading, Speed, Longitude, Latitude)"
-                            + " values (?, ?, ?, ?)";
+                        if (ContextCompat.checkSelfPermission(help, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) { // coarse permission is granted
+                            //Location loc = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
 
-                    // create the mysql insert preparedstatement
-                    PreparedStatement preparedStmt = conn.prepareStatement(query);
-                    preparedStmt.setInt (1, 't');
-                    preparedStmt.setFloat (2, 3);
-                    loc = mMap.getMyLocation();
-                    preparedStmt.setDouble (3, loc.getLongitude());
-                    preparedStmt.setDouble (4, loc.getLatitude());
+                            // the mysql insert statement
+                            for(int i=0; i<100; i++) {
+                                String query = " insert into Test (Heading, Speed, Longitude, Latitude)"
+                                        + " values (?, ?, ?, ?)";
 
-                    String result = "Database Connection Successful\n";
-                    preparedStmt.execute();
+                                // create the mysql insert prepared statement
+                                PreparedStatement preparedStmt = conn.prepareStatement(query);
+                                if(update) {
+                                    preparedStmt.setInt(1, 2);
+                                    preparedStmt.setFloat(2, 3);
+                                    preparedStmt.setDouble(3, -82.565981);
+                                    preparedStmt.setDouble(4, 35.616759);
 
-                    res = result;
+                                    preparedStmt.execute();
+                                }else{
+                                    preparedStmt.setInt(1, 2);
+                                    preparedStmt.setFloat(2, 3);
+                                    preparedStmt.setDouble(3, -82.565981);
+                                    preparedStmt.setDouble(4, 35.616759);
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    res = e.toString();
+                                    preparedStmt.execute();
+                                }
+
+                            }
+                            try {
+                                conn.close();
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        } else { // permission is not granted, request for permission
+                            if (ActivityCompat.shouldShowRequestPermissionRationale(help, Manifest.permission.ACCESS_COARSE_LOCATION)) { // show some info to user why you want this permission
+                                //Toast.makeText(this, "Allow Location Permission to use this functionality.", Toast.LENGTH_SHORT).show();
+                                ActivityCompat.requestPermissions(help, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 123 /*LOCATION_PERMISSION_REQUEST_CODE*/);
+                            } else {
+                                ActivityCompat.requestPermissions(help, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 123 /*LOCATION_PERMISSION_REQUEST_CODE*/);
+
+                            }
+                        }
+
+                        res = result;
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        res = e.toString();
+                    }
+                    return res;
+
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
                 }
-                return res;
-
-            } catch (ClassNotFoundException ex) {
-                ex.printStackTrace();
-            }
-            System.out.println("Data base selection success");
-
+                System.out.println("Data base selection success");
 
             return null;
         }
